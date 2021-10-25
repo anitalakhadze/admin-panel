f727017f423bfe7fc146d26b7f4e7c3fab9b1169
atala15 <atala15@freeuni.edu.ge>
2021-10-26 01:30:46 +0400

    Updated security config with a custom authentication filter.

--- src/main/java/com/example/demoadminpanel/security/SecurityConfig.java
+++ src/main/java/com/example/demoadminpanel/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.example.demoadminpanel.security;
 
+import com.example.demoadminpanel.security.filter.CustomAuthenticationFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -21,14 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "localhost:8080/user/**")
-                .authenticated().and()
-                .formLogin()
-                    .permitAll().and()
-                .logout()
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true).permitAll();
+                .sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+//                .authorizeRequests()
+//                    .antMatchers(HttpMethod.POST, "localhost:8080/user/**")
+//                .authenticated().and()
+//                .formLogin()
+//                    .permitAll().and()
+//                .logout()
+//                    .invalidateHttpSession(true)
+//                    .clearAuthentication(true).permitAll();
     }
 
     @Override
@@ -41,5 +50,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(bCryptPasswordEncoder);
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
