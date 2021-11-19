4ea65702aa04f94b4f48d8fdd94353d4904443bd
a.talakhadze <a.talakhadze@oppa.ge>
2021-11-19 11:15:33 +0400

    Minor Edits

--- src/main/java/com/example/demoadminpanel/security/utils/TokenUtils.java
+++ src/main/java/com/example/demoadminpanel/security/utils/TokenUtils.java
@@ -18,8 +18,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TokenUtils {
-    private static final int ACCESS_TOKEN_EXPIRATION_PERIOD = 10 * 60 * 1000;
-    private static final int REFRESH_TOKEN_EXPIRATION_PERIOD = 30 * 60 * 1000;
+    private static final int ACCESS_TOKEN_EXPIRATION_PERIOD = 30 * 60 * 1000;
+    private static final int REFRESH_TOKEN_EXPIRATION_PERIOD = 50 * 60 * 1000;
     private static final String ACCESS_TOKEN_HEADER_KEY = "access_token";
     private static final String REFRESH_TOKEN_HEADER_KEY = "refresh_token";
     private static final String SECRET_KEY = "someSecretKey";
