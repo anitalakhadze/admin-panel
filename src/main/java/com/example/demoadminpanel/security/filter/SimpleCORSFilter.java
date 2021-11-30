5d07533597965d6522558276790bf8d0625f4b73
a.talakhadze <a.talakhadze@oppa.ge>
2021-11-30 23:08:51 +0400

    updated pom with validation library and added put method to cors filter.

--- src/main/java/com/example/demoadminpanel/security/filter/SimpleCORSFilter.java
+++ src/main/java/com/example/demoadminpanel/security/filter/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, authorization");
 
