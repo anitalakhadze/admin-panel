f7f86856cca5e896c2506a1ccae4b21b430e0b2b
anita <talakhadzeani@gmail.com>
2023-12-29 16:34:11 +0200

    Setup data so that the all users are cleaned and the first admin user is created on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO admin_panel_users (name, username, password, ip_address, return_url, role, status, registered_at)
-VALUES ('admin', 'admin', 'password', '127.0.0.1', 'https://example.com', 'ADMIN', 'ACTIVE', CURRENT_TIMESTAMP);
+DELETE FROM admin_panel.admin_panel_users;
+
+INSERT INTO admin_panel.admin_panel_users (name, username, password, ip_address, return_url, role, status, registered_at)
+VALUES ('admin', 'admin', '$2a$10$Kp8vTSIzOUEpppuP6.nM4O/VNcxKBWPtVx6GjZt4aswdDEDsUg/.q', '127.0.0.1', 'https://example.com', 'ROLE_ADMIN', 'ACTIVE', CURRENT_TIMESTAMP);
