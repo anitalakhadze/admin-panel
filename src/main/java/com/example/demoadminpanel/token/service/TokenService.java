2cb245427fb52d9c74d958b35234ac4757371a6f
anita <talakhadzeani@gmail.com>
2024-01-10 23:28:38 +0400

    Remove unnecessary interface layer

--- src/main/java/com/example/demoadminpanel/token/service/TokenService.java
+++ src/main/java/com/example/demoadminpanel/token/service/TokenService.java
@@ -1,10 +1,42 @@
 package com.example.demoadminpanel.token.service;
 
+import com.example.demoadminpanel.security.utils.TokenUtils;
+import com.example.demoadminpanel.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public interface TokenService {
-    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
+@Service
+@AllArgsConstructor
+public class TokenService {
+
+    private final UserRepository userRepository;
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                Map<String, String> tokens = TokenUtils.refreshToken(authorizationHeader, userRepository, request);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception exception) {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                Map<String, String> errors = new HashMap<>();
+                errors.put("error_message", exception.getMessage());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), errors);
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
+    }
 }
